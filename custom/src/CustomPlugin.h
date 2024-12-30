1caf655e3bf55c7b2e1b2fe63d2be2b2bfa0f17d
Kaktusowy500 <k.lewand500@gmail.com>
2024-12-30 22:39:17 +0000

    Delete needless files and method to make a minimal plugin instance

--- custom/src/CustomPlugin.h
+++ custom/src/CustomPlugin.h
@@ -18,63 +18,22 @@
 
 #include <QTranslator>
 
-class CustomOptions;
 class CustomPlugin;
 class CustomSettings;
 
 Q_DECLARE_LOGGING_CATEGORY(CustomLog)
-
-class CustomFlyViewOptions : public QGCFlyViewOptions
-{
-public:
-    CustomFlyViewOptions(CustomOptions* options, QObject* parent = nullptr);
-
-    // Overrides from CustomFlyViewOptions
-    bool                    showInstrumentPanel         (void) const final;
-    bool                    showMultiVehicleList        (void) const final;
-};
-
-class CustomOptions : public QGCOptions
-{
-public:
-    CustomOptions(CustomPlugin*, QObject* parent = nullptr);
-
-    // Overrides from QGCOptions
-    bool                    wifiReliableForCalibration  (void) const final;
-    bool                    showFirmwareUpgrade         (void) const final;
-    QGCFlyViewOptions*      flyViewOptions(void) final;
-
-private:
-    CustomFlyViewOptions* _flyViewOptions = nullptr;
-};
 
 class CustomPlugin : public QGCCorePlugin
 {
     Q_OBJECT
 public:
     CustomPlugin(QGCApplication* app, QGCToolbox *toolbox);
+    bool mavlinkMessage(Vehicle* vehicle, LinkInterface* link, mavlink_message_t message) override;
+
     ~CustomPlugin();
-
-    // Overrides from QGCCorePlugin
-    QVariantList&           settingsPages                   (void) final;
-    QGCOptions*             options                         (void) final;
-    QString                 brandImageIndoor                (void) const final;
-    QString                 brandImageOutdoor               (void) const final;
-    bool                    overrideSettingsGroupVisibility (QString name) final;
-    bool                    adjustSettingMetaData           (const QString& settingsGroup, FactMetaData& metaData) final;
-    void                    paletteOverride                 (QString colorName, QGCPalette::PaletteColorInfo_t& colorInfo) final;
-    QQmlApplicationEngine*  createQmlApplicationEngine      (QObject* parent) final;
-
-    // Overrides from QGCTool
-    void                    setToolbox                      (QGCToolbox* toolbox);
-
 private slots:
-    void _advancedChanged(bool advanced);
 
 private:
-    void _addSettingsEntry(const QString& title, const char* qmlFile, const char* iconFile = nullptr);
 
 private:
-    CustomOptions*  _options = nullptr;
-    QVariantList    _customSettingsList; // Not to be mixed up with QGCCorePlugin implementation
 };
